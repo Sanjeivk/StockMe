61bb3781adfc67ff3fc1a42ed40ab69c42a6ac9f
Sanjeivk <sanjeiv411@gmail.com>
2021-10-11 01:51:17 -0400

    Header__Left
    Starting with the left side of the Header. Still need to fix the error where the size of the logo is not changing.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBQJtswDfgL8C2j1xPhUmuwWeTZ1kPzXIA",
+    authDomain: "stockme-c1bd7.firebaseapp.com",
+    projectId: "stockme-c1bd7",
+    storageBucket: "stockme-c1bd7.appspot.com",
+    messagingSenderId: "1044834148374",
+    appId: "1:1044834148374:web:2865d42f2a7740555e5537",
+    measurementId: "G-9ZCDT682N5"
+  };
